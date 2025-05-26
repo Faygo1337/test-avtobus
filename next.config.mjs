f7fecea6a6f8ceccdb16c619fb91b65b8c19b1ff
[Faygo1337] <[maximgustynov@gmail.com]>
2025-05-26 11:36:15 +0300

    fix: fixed deploy bugs

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,16 @@
 /** @type {import('next').NextConfig} */
-
-// Проверяем, находимся ли мы в окружении Vercel или делаем статический экспорт
-const isVercel = process.env.VERCEL === "1"; // Переменная, которую устанавливает Vercel
+const isVercel = process.env.VERCEL === "1";
 
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  // output: "export", // Убираем отсюда
-
-  // Применяем output, basePath и assetPrefix только если не на Vercel
   output: isVercel ? undefined : "export",
-  basePath: isVercel ? "" : "/test-avtobus", // basePath должен быть пустым на Vercel
-  assetPrefix: isVercel ? "" : "/test-avtobus/", // assetPrefix должен быть пустым на Vercel
+  basePath: isVercel ? "" : "/test-avtobus",
+  assetPrefix: isVercel ? "" : "/test-avtobus/",
 
   images: {
     domains: [],
     formats: ["image/avif", "image/webp"],
-    // Если ты используешь Image из 'next/image' и деплоишь на GitHub Pages,
-    // возможно потребуется добавить unoptimized: true
-    // unoptimized: isGithubPages || !isVercel,
   },
   experimental: {
     optimizeCss: true,
